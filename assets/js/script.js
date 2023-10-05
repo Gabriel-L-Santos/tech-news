7fc9f76326678c6adfa8f8761df5085d953f70c5
Gabriel Leme dos Santos <gabriellemesantos@gmail.com>
2023-10-05 12:07:10 -0300

    iniciando o header

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+// Menu Hamburguer
+
+const menuHamburguer = document.querySelector('.menu-hamburguer');
+const nav = document.querySelector('.navbar');
+const links = document.querySelectorAll('.navbar-links li a');
+
+menuHamburguer.addEventListener('click', () => {
+    nav.classList.toggle('active');
+});
+
+links.forEach(item => {
+    item.addEventListener('click', () => {
+        nav.classList.toggle('active');
+    });
+});
+
+// Efeito fade-in scroll da página
+
+let header = document.getElementById('header');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY >= 200) {
+        header.style.background = '#191919'
+    } else {
+        header.style.background = 'transparent'
+    }
+
+});   
